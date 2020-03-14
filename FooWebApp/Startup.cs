4dce05a3ef9cca478f0dac317f383d51ba4a3fbc
Nehme Bilal <nehme.bilal@earnin.com>
2020-03-14 12:57:19 +0200

    Manually add the instrumentation key

--- FooWebApp/Startup.cs
+++ FooWebApp/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FooWebApp
 {
@@ -18,8 +19,11 @@ namespace FooWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApplicationInsightsTelemetry();
             services.AddControllers();
+
+            string instrumentationKey = Configuration["ApplicationInsights:InstrumentationKey"];
+            services.AddApplicationInsightsTelemetry(instrumentationKey);
+            
 
             services.AddSingleton<IStudentStore, AzureTableStudentStore>();
 
@@ -28,7 +32,7 @@ namespace FooWebApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
